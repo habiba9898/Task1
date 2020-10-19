d52f7886e3b0f82c8457b2f0371531232e0792a5
habiba9898 <$ ssh-keygen -t rsa -b 4096 -C "habibamansour163@gmail.com>
2020-10-19 10:52:40 +0200

    Updates

---
+++ precache-manifest.44687a3fc3bff476d5fa88cf98b4d218.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb2cab72418660786b59e7027121b68",
+    "url": "/Task1/index.html"
+  },
+  {
+    "revision": "c24db34862197470e615",
+    "url": "/Task1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2eb9044568c04d4db2d",
+    "url": "/Task1/static/js/2.a729f157.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Task1/static/js/2.a729f157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24db34862197470e615",
+    "url": "/Task1/static/js/main.99cc645a.chunk.js"
+  },
+  {
+    "revision": "00dede68efcd1c025a1e",
+    "url": "/Task1/static/js/runtime-main.3e402149.js"
+  },
+  {
+    "revision": "95cdf29053205f789b949d17e9908d94",
+    "url": "/Task1/static/media/back1.95cdf290.png"
+  },
+  {
+    "revision": "443a0c7b660742ca79c0f33d4ecfff8d",
+    "url": "/Task1/static/media/back2.443a0c7b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Task1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ba3b9ac940e2ce8773d1a94b95fac4a",
+    "url": "/Task1/static/media/output-onlinepngtools.3ba3b9ac.png"
+  }
+]);
